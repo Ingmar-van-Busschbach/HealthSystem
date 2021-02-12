53c74a5344692cf3fcf17a6043d966620b018e1a
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-02-12 12:09:02 +0100

    Updated and reworked imported scripts

---
+++ Assets/Scripts/ClickDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDamage : DamageSystem
+{
+    [SerializeField] private bool buttonPressed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && buttonPressed == false)
+        {
+            buttonPressed = true;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 1000))
+            {
+                //Debug.Log(hit.transform.gameObject);
+                if(hit.transform.gameObject != null)
+                {
+                    ApplyDamage(_damage, hit.transform.gameObject);
+                }
+            }
+        }
+        if (!Input.GetButton("Fire1"))
+        {
+            buttonPressed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    [SerializeField] protected float _damage = 10.0f;
+
+    protected virtual void ApplyDamage(float damage, GameObject target)
+    {
+        DamageEvents.current.ApplyDamage(damage, target);
+    }
+}
