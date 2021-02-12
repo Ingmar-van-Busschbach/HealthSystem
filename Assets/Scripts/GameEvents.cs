19f69237bfe621485ef5480e7dbb256b12734160
Ingmar-van-Busschbach <TheCatPlaysGamez@gmail.com>
2021-02-12 10:59:24 +0100

    Added prototype scripts

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action<int> onDealPlayerDamage;
+    public void DealPlayerDamage(int damage = 0)
+    {
+        if (onDealPlayerDamage != null)
+        {
+            onDealPlayerDamage(damage);
+        }
+    }
+    public event Action<int> onDealEnemyDamage;
+    public void DealEnemyDamage(int damage = 0)
+    {
+        if (onDealEnemyDamage != null)
+        {
+            onDealEnemyDamage(damage);
+        }
+    }
+    public event Action<int> onPlayerScore;
+    public void AddPlayerScore(int score = 0)
+    {
+        if (onDealEnemyDamage != null)
+        {
+            onPlayerScore(score);
+        }
+    }
+    public event Action<int> onPlayerGameOver;
+    public void PlayerGameOver(int score = 0)
+    {
+        onPlayerGameOver(score);
+    }
+}
